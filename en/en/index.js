286be67a79230c742c960f292fe42be8f736abd1
wangyan666 <90406579+wangyan666@users.noreply.github.com>
2023-02-10 13:56:52 +0800

    New translations index.js (English)

--- en/en/index.js
+++ en/en/index.js
@@ -1,2 +1,22 @@
 const a = 'Menu';
 const b = 'Print Statistics';
+const createPixelMap = ctx => {
+    let pxMap = [];
+    for (let i = 0; i < totalWidth.value; i += sizeInfo.size) {
+        for (let j = 0; j < totalHeight.value; j += sizeInfo.size) {
+            let pixel = ctx.getImageData(i, j, 1, 1).data;
+            let color = `rgba(${ pixel[0] },${ pixel[1] },${ pixel[2] },${ pixel[3] / 255 })`;
+            pxMap.push({
+                x: i / sizeInfo.size,
+                y: j / sizeInfo.size,
+                color
+            });
+        }
+    }
+    let s = {
+        a: '技术文档',
+        b: '联系我们',
+        c: '查看演示demo'
+    };
+    return pxMap;
+};
